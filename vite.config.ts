1340c9f436f71a063a0cbb998ea5cb2bf71d617c
zengxiangfu <zengxiangfu@zhuanzhuan.com>
2022-09-19 13:45:27 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { rmSync } from 'fs';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import electron from 'vite-plugin-electron';
+import pkg from './package.json';
+
+rmSync('dist', { recursive: true, force: true });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            sourcemap: false,
+            outDir: 'dist/electron/main',
+          },
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: path.join(__dirname, 'electron/preload/index.ts'),
+        },
+        vite: {
+          build: {
+            // For debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          },
+        },
+      },
+    }),
+  ],
+  server: {
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+});
